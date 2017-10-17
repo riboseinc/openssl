045627e28dddeea844d6224909915174989b86ab
Jack Lloyd <jack@randombit.net>
2017-10-17 13:26:16 -0400

    Use large table for SM4 sbox

--- crypto/sm4/sm4.c
+++ crypto/sm4/sm4.c
@@ -37,6 +37,54 @@ static const uint8_t SM4_S[256] = {
     0xD7, 0xCB, 0x39, 0x48
 };
 
+/*
+* SM4_SBOX_T[j] == L(SM4_SBOX[j]).
+*/
+const uint32_t SM4_SBOX_T[256] = {
+   0x8ED55B5B, 0xD0924242, 0x4DEAA7A7, 0x06FDFBFB, 0xFCCF3333, 0x65E28787,
+   0xC93DF4F4, 0x6BB5DEDE, 0x4E165858, 0x6EB4DADA, 0x44145050, 0xCAC10B0B,
+   0x8828A0A0, 0x17F8EFEF, 0x9C2CB0B0, 0x11051414, 0x872BACAC, 0xFB669D9D,
+   0xF2986A6A, 0xAE77D9D9, 0x822AA8A8, 0x46BCFAFA, 0x14041010, 0xCFC00F0F,
+   0x02A8AAAA, 0x54451111, 0x5F134C4C, 0xBE269898, 0x6D482525, 0x9E841A1A,
+   0x1E061818, 0xFD9B6666, 0xEC9E7272, 0x4A430909, 0x10514141, 0x24F7D3D3,
+   0xD5934646, 0x53ECBFBF, 0xF89A6262, 0x927BE9E9, 0xFF33CCCC, 0x04555151,
+   0x270B2C2C, 0x4F420D0D, 0x59EEB7B7, 0xF3CC3F3F, 0x1CAEB2B2, 0xEA638989,
+   0x74E79393, 0x7FB1CECE, 0x6C1C7070, 0x0DABA6A6, 0xEDCA2727, 0x28082020,
+   0x48EBA3A3, 0xC1975656, 0x80820202, 0xA3DC7F7F, 0xC4965252, 0x12F9EBEB,
+   0xA174D5D5, 0xB38D3E3E, 0xC33FFCFC, 0x3EA49A9A, 0x5B461D1D, 0x1B071C1C,
+   0x3BA59E9E, 0x0CFFF3F3, 0x3FF0CFCF, 0xBF72CDCD, 0x4B175C5C, 0x52B8EAEA,
+   0x8F810E0E, 0x3D586565, 0xCC3CF0F0, 0x7D196464, 0x7EE59B9B, 0x91871616,
+   0x734E3D3D, 0x08AAA2A2, 0xC869A1A1, 0xC76AADAD, 0x85830606, 0x7AB0CACA,
+   0xB570C5C5, 0xF4659191, 0xB2D96B6B, 0xA7892E2E, 0x18FBE3E3, 0x47E8AFAF,
+   0x330F3C3C, 0x674A2D2D, 0xB071C1C1, 0x0E575959, 0xE99F7676, 0xE135D4D4,
+   0x661E7878, 0xB4249090, 0x360E3838, 0x265F7979, 0xEF628D8D, 0x38596161,
+   0x95D24747, 0x2AA08A8A, 0xB1259494, 0xAA228888, 0x8C7DF1F1, 0xD73BECEC,
+   0x05010404, 0xA5218484, 0x9879E1E1, 0x9B851E1E, 0x84D75353, 0x00000000,
+   0x5E471919, 0x0B565D5D, 0xE39D7E7E, 0x9FD04F4F, 0xBB279C9C, 0x1A534949,
+   0x7C4D3131, 0xEE36D8D8, 0x0A020808, 0x7BE49F9F, 0x20A28282, 0xD4C71313,
+   0xE8CB2323, 0xE69C7A7A, 0x42E9ABAB, 0x43BDFEFE, 0xA2882A2A, 0x9AD14B4B,
+   0x40410101, 0xDBC41F1F, 0xD838E0E0, 0x61B7D6D6, 0x2FA18E8E, 0x2BF4DFDF,
+   0x3AF1CBCB, 0xF6CD3B3B, 0x1DFAE7E7, 0xE5608585, 0x41155454, 0x25A38686,
+   0x60E38383, 0x16ACBABA, 0x295C7575, 0x34A69292, 0xF7996E6E, 0xE434D0D0,
+   0x721A6868, 0x01545555, 0x19AFB6B6, 0xDF914E4E, 0xFA32C8C8, 0xF030C0C0,
+   0x21F6D7D7, 0xBC8E3232, 0x75B3C6C6, 0x6FE08F8F, 0x691D7474, 0x2EF5DBDB,
+   0x6AE18B8B, 0x962EB8B8, 0x8A800A0A, 0xFE679999, 0xE2C92B2B, 0xE0618181,
+   0xC0C30303, 0x8D29A4A4, 0xAF238C8C, 0x07A9AEAE, 0x390D3434, 0x1F524D4D,
+   0x764F3939, 0xD36EBDBD, 0x81D65757, 0xB7D86F6F, 0xEB37DCDC, 0x51441515,
+   0xA6DD7B7B, 0x09FEF7F7, 0xB68C3A3A, 0x932FBCBC, 0x0F030C0C, 0x03FCFFFF,
+   0xC26BA9A9, 0xBA73C9C9, 0xD96CB5B5, 0xDC6DB1B1, 0x375A6D6D, 0x15504545,
+   0xB98F3636, 0x771B6C6C, 0x13ADBEBE, 0xDA904A4A, 0x57B9EEEE, 0xA9DE7777,
+   0x4CBEF2F2, 0x837EFDFD, 0x55114444, 0xBDDA6767, 0x2C5D7171, 0x45400505,
+   0x631F7C7C, 0x50104040, 0x325B6969, 0xB8DB6363, 0x220A2828, 0xC5C20707,
+   0xF531C4C4, 0xA88A2222, 0x31A79696, 0xF9CE3737, 0x977AEDED, 0x49BFF6F6,
+   0x992DB4B4, 0xA475D1D1, 0x90D34343, 0x5A124848, 0x58BAE2E2, 0x71E69797,
+   0x64B6D2D2, 0x70B2C2C2, 0xAD8B2626, 0xCD68A5A5, 0xCB955E5E, 0x624B2929,
+   0x3C0C3030, 0xCE945A5A, 0xAB76DDDD, 0x867FF9F9, 0xF1649595, 0x5DBBE6E6,
+   0x35F2C7C7, 0x2D092424, 0xD1C61717, 0xD66FB9B9, 0xDEC51B1B, 0x94861212,
+   0x78186060, 0x30F3C3C3, 0x897CF5F5, 0x5CEFB3B3, 0xD23AE8E8, 0xACDF7373,
+   0x794C3535, 0xA0208080, 0x9D78E5E5, 0x56EDBBBB, 0x235E7D7D, 0xC63EF8F8,
+   0x8BD45F5F, 0xE7C82F2F, 0xDD39E4E4, 0x68492121 };
+
 static inline uint32_t rotl(uint32_t a, uint8_t n)
 {
     return (a << n) | (a >> (32 - n));
@@ -45,8 +93,9 @@ static inline uint32_t rotl(uint32_t a, uint8_t n)
 static inline uint32_t load_u32_be(const uint8_t *b, uint32_t n)
 {
     return ((uint32_t)b[4 * n] << 24) ^
-        ((uint32_t)b[4 * n + 1] << 16) ^
-        ((uint32_t)b[4 * n + 2] << 8) ^ ((uint32_t)b[4 * n + 3]);
+           ((uint32_t)b[4 * n + 1] << 16) ^
+           ((uint32_t)b[4 * n + 2] << 8) ^
+           ((uint32_t)b[4 * n + 3]);
 }
 
 static inline void store_u32_be(uint32_t v, uint8_t *b)
@@ -57,13 +106,8 @@ static inline void store_u32_be(uint32_t v, uint8_t *b)
     b[3] = (v) & 0xFF;
 }
 
-static inline uint32_t SM4_T(uint32_t X)
+static inline uint32_t SM4_T_slow(uint32_t X)
 {
-    /*
-       Could convert this to 4 8->32 bit tables combined with xor. The
-       large tables would be vulnerable to AES style cache attacks though.
-     */
-
     uint32_t t = 0;
     t |= ((uint32_t)SM4_S[(X >> 24) & 0xFF]) << 24;
     t |= ((uint32_t)SM4_S[(X >> 16) & 0xFF]) << 16;
@@ -72,6 +116,18 @@ static inline uint32_t SM4_T(uint32_t X)
 
     // L linear transform
     return t ^ rotl(t, 2) ^ rotl(t, 10) ^ rotl(t, 18) ^ rotl(t, 24);
+}
+
+static inline uint32_t SM4_T(uint32_t X)
+{
+//return SM4_T_slow(X);
+    uint32_t t = 0;
+    t ^= SM4_SBOX_T[(X >> 24) & 0xFF];
+    t ^= rotl(SM4_SBOX_T[(X >> 16) & 0xFF], 24);
+    t ^= rotl(SM4_SBOX_T[(X >> 8) & 0xFF], 16);
+    t ^= rotl(SM4_SBOX_T[X & 0xFF], 8);
+
+    return t;
 }
 
 int SM4_set_key(const uint8_t *key, SM4_KEY * ks)
@@ -115,6 +171,13 @@ int SM4_set_key(const uint8_t *key, SM4_KEY * ks)
     return 1;
 }
 
+#define SM4_RNDS(k0,k1,k2,k3,F) do {         \
+         B0 ^= F(B1 ^ B2 ^ B3 ^ ks->rk[k0]); \
+         B1 ^= F(B0 ^ B2 ^ B3 ^ ks->rk[k1]); \
+         B2 ^= F(B0 ^ B1 ^ B3 ^ ks->rk[k2]); \
+         B3 ^= F(B0 ^ B1 ^ B2 ^ ks->rk[k3]); \
+      } while(0)
+
 void SM4_encrypt(const uint8_t *in, uint8_t *out, const SM4_KEY * ks)
 {
     uint32_t B0 = load_u32_be(in, 0);
@@ -122,12 +185,18 @@ void SM4_encrypt(const uint8_t *in, uint8_t *out, const SM4_KEY * ks)
     uint32_t B2 = load_u32_be(in, 2);
     uint32_t B3 = load_u32_be(in, 3);
 
-    for (uint8_t r = 0; r != 32; r += 4) {
-        B0 ^= SM4_T(B1 ^ B2 ^ B3 ^ ks->rk[r + 0]);
-        B1 ^= SM4_T(B0 ^ B2 ^ B3 ^ ks->rk[r + 1]);
-        B2 ^= SM4_T(B0 ^ B1 ^ B3 ^ ks->rk[r + 2]);
-        B3 ^= SM4_T(B0 ^ B1 ^ B2 ^ ks->rk[r + 3]);
-    }
+    /*
+    * Uses byte-wise sbox in the first and last rounds to provide some
+    * protection from cache based side channels.
+    */
+    SM4_RNDS( 0,  1,  2,  3, SM4_T_slow);
+    SM4_RNDS( 4,  5,  6,  7, SM4_T);
+    SM4_RNDS( 8,  9, 10, 11, SM4_T);
+    SM4_RNDS(12, 13, 14, 15, SM4_T);
+    SM4_RNDS(16, 17, 18, 19, SM4_T);
+    SM4_RNDS(20, 21, 22, 23, SM4_T);
+    SM4_RNDS(24, 25, 26, 27, SM4_T);
+    SM4_RNDS(28, 29, 30, 31, SM4_T_slow);
 
     store_u32_be(B3, out);
     store_u32_be(B2, out + 4);
@@ -142,12 +211,14 @@ void SM4_decrypt(const uint8_t *in, uint8_t *out, const SM4_KEY * ks)
     uint32_t B2 = load_u32_be(in, 2);
     uint32_t B3 = load_u32_be(in, 3);
 
-    for (uint8_t r = 0; r != 32; r += 4) {
-        B0 ^= SM4_T(B1 ^ B2 ^ B3 ^ ks->rk[31 - (r + 0)]);
-        B1 ^= SM4_T(B0 ^ B2 ^ B3 ^ ks->rk[31 - (r + 1)]);
-        B2 ^= SM4_T(B0 ^ B1 ^ B3 ^ ks->rk[31 - (r + 2)]);
-        B3 ^= SM4_T(B0 ^ B1 ^ B2 ^ ks->rk[31 - (r + 3)]);
-    }
+    SM4_RNDS(31, 30, 29, 28, SM4_T_slow);
+    SM4_RNDS(27, 26, 25, 24, SM4_T);
+    SM4_RNDS(23, 22, 21, 20, SM4_T);
+    SM4_RNDS(19, 18, 17, 16, SM4_T);
+    SM4_RNDS(15, 14, 13, 12, SM4_T);
+    SM4_RNDS(11, 10,  9,  8, SM4_T);
+    SM4_RNDS( 7,  6,  5,  4, SM4_T);
+    SM4_RNDS( 3,  2,  1,  0, SM4_T_slow);
 
     store_u32_be(B3, out);
     store_u32_be(B2, out + 4);
